ef639e546593f188e561d9c13bd10f7d5a3b9049
radheyshyam <103444174+raju11shi@users.noreply.github.com>
2022-04-11 22:21:10 +0530

    Update streamlit_app.py
    corrected the lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healty Diner')
-streamlit.header("My Mom's New Healthy Diner")
-streamlit.text('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale , Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
